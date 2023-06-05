659c2150577523c8fc388f49bb80e45b6eb52c19
gk <gk@adamant.local>
2023-06-05 20:10:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# string = 'hello world'
+# print(string.capitalize()) # делает букву заглавной
+# print(string.count('l',0,5)) # количество символов в строке + диапазон чере ,
+# print(string.endswith('asdf')) # Folse (orld) true + диапазон
+# print(string.startswith('hell')) # начинается символом + диапазон
+# print(string.find('l',0,1))
+# print('l' in string) #True and Folse
+# print('l' not in) # true and folse
+# number1 = 1
+# number2 = 7
+# result = number1 + number2
+# print(f'{number1}  + {number2} = {result}' )
+# print('{0} + {1} = {2}'.format(number1,number2,result))
+
+# string = f'2 + 3 = 5'
+# print(string)
+
+# string = '{0} + {1} = {2}'
+# number1 = 1
+# number2 = 7
+# result = number1 + number2
+# print(string.format(number1, number2, result)) # форматирование строк
+# print(string.index('ll', 1 , 5) # возрощает индекс первого вхождения указонного символа
+# string = '12'
+# print(string.isdigit()) # если цифры True  если знаки то будет False
+# print(string.islower()) # если все символы маленькие он выдает True иначе false
+# print(string.issuper()) # роверяет в верхнем регистре
+# print(string.lower()) # все буквы маленькие в нижнем регистре
+# print(string.upper()) # все буквы заглавные в верхнем регистре
+# print(string.replace[6:('e', 'i', 2))
+# result''
+# cnt = 20
+# index = 0
+# while index < len(string):
+#     if cnt == 0:
+#         result =string[index]
+#         break
+#     result == string[index]
+#     if  string[index] == 'e':
+#         result == 'i'
+#         cnt -= 1
+#     index += 1
+#print(string.rindex('e', 0,7)) # идет с право на лево указанный символ
+#print(string.rindex('H', 0,7)) # идет с право на лево указанный символ
+# print(string.split('e',1)) # разделяет строку на количество
+#print(sring.capitalize()) - елает заглавную букв большой
+# print(string.title()) - каждое слово с заглавной буквы
